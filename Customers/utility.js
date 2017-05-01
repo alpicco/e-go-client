45f03705242ab4cf2d9aba9b6dab9d86a3d53627
Alessandro <mpiccoli2958@gmail.com>
2017-05-01 11:32:00 +0200

    Link product-detail view with selected product.

--- Customers/utility.js
+++ Customers/utility.js
@@ -80,22 +80,12 @@ function parseItem(data) {
     var shippable = value["shippable"];
     var images = value["images"];
 
-    var image = new Image();
-    image.src = images;
-    if(image.height > image.width) {
-      image.style.height = '80px';
-      image.style.width = 'auto';
-    } else {
-      image.style.width = '80px';
-      image.style.height = 'auto';
-    }
-    
     var html = ['<div class="container>',
-     '<div class="row" id ="cont">',
-     '<div class="col-md-3">',
+      '<div class="row" id ="cont">',
+      '<div class="col-md-3">',
       '<div class="ibox">',
       '<div class="ibox-content product-box">',
-      '<div class="product-imitation" style="background-image: url('+image.src+');background-size: cover;">',
+      '<div class="product-imitation" style="background-image: url(' + images + ');background-size: cover;">',
       '</div>',
       '<div class="product-desc">',
       '<span class="product-price">12$',
@@ -106,7 +96,7 @@ function parseItem(data) {
       '<div class="small m-t-xs">' + description + '',
       '</div>',
       '<div class="m-t text-right">',
-      '<a href="product_detail.html" class="btn btn-xs btn-outline btn-primary">Info <i class="fa fa-long-arrow-right"></i> </a>',
+      '<a href="product_detail.html" onclick="return productDetail(\'' + name + '\', \'' + caption + '\', \'' + description + '\', \'' + shippable + '\', \'' + images + '\')" class="btn btn-xs btn-outline btn-primary">Info</a>',
       '</div>',
       '</div>',
       '</div>',
@@ -118,4 +108,33 @@ function parseItem(data) {
     div.innerHTML = html;
     document.getElementsByTagName("body")[0].appendChild(div);
   });
+}
+
+function productDetail(name, caption, description, shippable, images) {
+  var html = ['<div class="container">',
+    '<div class="row">',
+    '<div class="col-xs-4 item-photo">',
+    '<img style="max-width:100%;" src="' + images + '"/>',
+    '</div>',
+    '<div class="col-xs-5" style="border:0px solid gray">',
+    '<h3>' + name + '</h3>',
+    '<h6 class="title-price"><small>PRICE</small></h6>',
+    '<h3 id="price" style="margin-top:0px;">$399</h3>',
+    '<div class="section" style="padding-bottom:20px;">',
+    '<h6 class="title-attr"><small>QUANTITY</small></h6>',
+    '<div>',
+    '<div class="btn-minus"><span class="glyphicon glyphicon-minus"></span></div>',
+    '<input value="1" />',
+    '<div class="btn-plus"><span class="glyphicon glyphicon-plus"></span></div>',
+    '</div>',
+    '</div>',
+    '<div class="section" style="padding-bottom:20px;">',
+    '<button class="btn btn-success"><span style="margin-right:10px" class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> Buy</button>',
+    '</div>',
+    '</div>',
+    '</div>',
+    '</div>'].join('');
+    var div = document.createElement('div');
+    div.innerHTML = html;
+    document.getElementById("detail").appendChild(div);
 }
