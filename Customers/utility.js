a4c70dc9adf38a5e9f2feecab500ad28a4e3a39e
Alessandro <mpiccoli2958@gmail.com>
2017-05-10 15:44:53 +0200

    Add call for adding to cart

--- Customers/utility.js
+++ Customers/utility.js
@@ -1,5 +1,5 @@
 function onSubmitRegister(form) {
-  var frm = $(form);
+  var frm = $("#form");
   var formData = frm.serializeArray();
 
   var myData = {};
@@ -72,52 +72,70 @@ function onLoadProductList() {
 }
 
 function parseItem(data) {
+  var $loading = $('#loadingDiv').hide();
+  $(document).ajaxStart(function () {
+      $loading.show();
+    })
+    .ajaxStop(function () {
+      $loading.hide();
+    });
   var json = JSON.parse(data);
   $.each(json, function (index, value) {
-    var name = value["name"];
-    var caption = value["caption"];
-    var description = value["description"];
-    var shippable = value["shippable"];
-    var images = value["images"];
-    var price = value["price"];
+    var name = value.name;
+    var caption = value.caption;
+    var description = value.description;
+    var url = value.skus.url;
+    var images = value.images;
+    var count = value.skus.total_count;
+    var skus = value.skus.data;
+    var price;
+    var sku;
+    $.each(skus, function (index, value) {
+      price = value["price"];
+      sku = value["id"];
+    });
 
-    var html = ['<div class="container>',
-      '<div class="row" id ="cont">',
-      '<div class="col-md-3">',
-      '<div class="ibox">',
-      '<div class="ibox-content product-box">',
-      '<div class="product-imitation" style="background-image: url(' + images + ');background-size: cover;">',
-      '</div>',
-      '<div class="product-desc">',
-      '<span class="product-price">' + price + '$',
-      '</span>',
-      '<small class="text-muted">' + caption + '</small>',
-      '<a href="product_detail.html" class="product-name">' + name + '</a>',
+    if (count > 0) {
+      var html = ['<div class="container>',
+        '<div class="row" id ="cont">',
+        '<div class="col-md-3">',
+        '<div class="ibox">',
+        '<div class="ibox-content product-box">',
+        '<div class="product-imitation" style="background-image: url(' + images + ');background-size: cover;">',
+        '</div>',
+        '<div class="product-desc">',
+        '<span class="product-price">' + price + '$',
+        '</span>',
+        '<small class="text-muted">' + caption + '</small>',
+        '<a href="product_detail.html" class="product-name">' + name + '</a>',
 
-      '<div class="small m-t-xs">' + description + '',
-      '</div>',
-      '<div class="m-t text-right">',
-      '<a href="product_detail.html?name=' + name + '&image=' + images + '&description=' + description + '" onclick="return productDetail()" class="btn btn-xs btn-outline btn-primary">Info</a>',
-      '</div>',
-      '</div>',
-      '</div>',
-      '</div>',
-      '</div>',
-      '</div>',
-      '</div>'].join('');
-    var div = document.createElement('div');
-    div.innerHTML = html;
-    document.getElementsByTagName("body")[0].appendChild(div);
+        '<div class="small m-t-xs">' + description + '',
+        '</div>',
+        '<div class="m-t text-right">',
+        '<a href="product_detail.html?name=' + name + '&image=' + images + '&description=' + description + '&sku=' + sku + '&count=' + count + '&price=' + price + '" onclick="return productDetail()" class="btn btn-xs btn-outline btn-primary">Info</a>',
+        '</div>',
+        '</div>',
+        '</div>',
+        '</div>',
+        '</div>',
+        '</div>',
+        '</div>'].join('');
+      var div = document.createElement('div');
+      div.innerHTML = html;
+      document.getElementsByTagName("body")[0].appendChild(div);
+    }
   });
   //\'' + name + '\', \'' + caption + '\', \'' + description + '\', \'' + shippable + '\', \'' + images + '\'
+  //
 }
 
 function productDetail() {
   var name = getParameterByName("name", window.location.href);
-  var image = getParameterByName("image", window.location.href);  
+  var image = getParameterByName("image", window.location.href);
   var description = getParameterByName("description", window.location.href);
-
-
+  var sku = getParameterByName("sku", window.location.href);
+  var count = getParameterByName("count", window.location.href);
+  var price = getParameterByName("price", window.location.href);
   var html = ['<div class="container">',
     '<div class="row">',
     '<div class="col-xs-4 item-photo">',
@@ -126,18 +144,18 @@ function productDetail() {
     '<div class="col-xs-5" style="border:0px solid gray">',
     '<h3>' + name + '</h3>',
     '<h6 class="title-price"><small>PRICE</small></h6>',
-    '<h3 id="price" style="margin-top:0px;">$399</h3>',
+    '<h3 id="price" style="margin-top:0px;">$' + price + '</h3>',
     '<div class="section" style="padding-bottom:20px;">',
     '<h6 class="title-attr"><small>QUANTITY</small></h6>',
     '<div>',
     '<div class="btn-minus" onclick="return sub();"><span class="glyphicon glyphicon-minus"></span></div>',
     '<input id="quantity" value="1" />',
-    '<div class="btn-plus" onclick="return add();"><span class="glyphicon glyphicon-plus"></span></div>',
+    '<div class="btn-plus" onclick="return add(' + count + ');"><span class="glyphicon glyphicon-plus"></span></div>',
     '</div>',
     '</div>',
     '<p>' + description + '</p>',
     '<div class="section" style="padding-bottom:20px;">',
-    '<button onclick="#" class="btn btn-success"><span style="margin-right:10px" class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> Add to cart</button>',
+    '<button onclick="return addToCart(\'' + sku + '\')" class="btn btn-success"><span style="margin-right:10px" class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> Add to cart</button>',
     '</div>',
     '</div>',
     '</div>',
@@ -155,4 +173,27 @@ function getParameterByName(name, url) {
   if (!results) return null;
   if (!results[2]) return '';
   return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function addToCart(sku) {
+  var token = JSON.parse(localStorage.getItem("token"));
+  var auth = token["token"];
+  var list = [sku];
+
+  $.ajax({
+    type: "POST",
+    url: "https://9839f3bb.ngrok.io/order",
+    async: false,
+    beforeSend: function (xhr) {
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.setRequestHeader("Authorization", "Bearer " + auth);
+    },
+    data: JSON.stringify(list),
+    success: function (data) {
+      alert("added");
+    },
+    error: function (xhr) {
+      alert(xhr.responseText);
+    }
+  });
 }
